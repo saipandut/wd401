449e2e5af9a2abade4bc7ebb68ea0204b5e5cd42
sai <pandusai025@gmail.com>
2023-09-28 11:46:01 +0530

    remove unwanted code

--- sai.js
+++ sai.js
@@ -1,22 +1,20 @@
-const express = require('express');
+ const express = require('express');
 const admin = require('firebase-admin');
-
 const app = express();
-const port = process.env.PORT || 3000; // Use process.env.PORT if available, or default to port 3000
-
+const bodyParser =require("body-parser");
+const bcrypt = require('bcrypt');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+const port = process.env.PORT || 3000; 
 app.use(express.static("views"));
-
 
 // Firebase configuration
 const serviceAccount = require("./Key.json");
-app.set('view engine', 'ejs'); // Set EJS as the view engine
-
+app.set('view engine', 'ejs'); 
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
-
-
 const db = admin.firestore();
 
 app.get("/signup",function(req,res){
@@ -24,32 +22,78 @@ app.get("/signup",function(req,res){
 });
 
 
-app.get("/signupSubmit",function(req,res){
-    console.log(req.query.name);
+/* app.post("/signupSubmit",function(req,res){
+    console.log(req.body.name);
    
     db.collection("EntriesList").add({
-        name:req.query.name,
-        email:req.query.email,
-        password:req.query.password,
-        mobileNo:req.query.mobileNo,
-       dob:req.query.dob,
+        name:req.body.name,
+        email:req.body.email,
+        password:req.body.password,
+        mobileNo:req.body.mobileNo,
+       dob:req.body.dob,
     }).then(()=>{
         res.redirect(`/dashboard?user=${req.query.name}`);
     });
 });
+*/
+
+app.post("/signupSubmit", async function (req, res) {
+    const name = req.body.name;
+    const email = req.body.email;
+    const plainPassword = req.body.password; // Get the plain text password from the request body
+
+    // Hash the password using bcrypt
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(plainPassword, saltRounds);
+
+    const mobileNo = req.body.mobileNo;
+    const dob = req.body.dob;
+
+    // Check if the email already exists in the database
+    const emailExists = await checkIfEmailExists(email);
+
+    if (emailExists) {
+        // Handle the case where the email is already registered
+        res.send("Email already registered. Please use a different email.");
+    } else {
+        // Store the hashed password in Firestore
+        db.collection("EntriesList")
+            .add({
+                name: name,
+                email: email,
+                password: hashedPassword, // Store the hashed password
+                mobileNo: mobileNo,
+                dob: dob,
+            })
+            .then(() => {
+                res.redirect(`/login`);
+            })
+            .catch((error) => {
+                console.error("Error adding document to Firestore:", error);
+                res.send("An error occurred. Please try again.");
+            });
+    }
+});
+
+// Function to check if the email already exists in the database
+async function checkIfEmailExists(email) {
+    const query = db.collection("EntriesList").where("email", "==", email).limit(1);
+    const snapshot = await query.get();
+    return !snapshot.empty;
+}
 
 
-app.get("/signinSubmit",function(req,res){
-    
+/*
+app.post("/signinSubmit",function(req,res){
    
     db.collection("EntriesList")
-    .where("email", "==", req.query.email)
-    .where("password", "==", req.query.password)
-    .where("dob", "==", req.query.dob)
+    .where("email", "==", req.body.email)
+    .where("password", "==", req.body.password)
+    .where("dob", "==", req.body.dob)
     .get()
     .then((docs)=>{
         if(docs.size>0){
-            res.redirect(`/dashboard?user=${req.query.name}`);
+            res.redirect(`/dashboard?`);
         }
         else{
             res.send("FAILED TO SIGIN PLEASE TRY AGAIN")
@@ -60,6 +104,40 @@ app.get("/signinSubmit",function(req,res){
         res.send("An error occurred. Please try again.");
     });
 });
+*/
+app.post("/signinSubmit", async function (req, res) {
+    const email = req.body.email;
+    const plainPassword = req.body.password;
+    const dob = req.body.dob;
+
+    try {
+        // Find the user with the given email address
+        const userQuery = await db.collection("EntriesList").where("email", "==", email).limit(1).get();
+
+        if (!userQuery.empty) {
+            const userDoc = userQuery.docs[0];
+            const userData = userDoc.data();
+            const hashedPassword = userData.password;
+
+            // Compare the hashed password with the provided plain password using bcrypt
+            const passwordMatch = await bcrypt.compare(plainPassword, hashedPassword);
+
+            if (passwordMatch && userData.dob === dob) {
+                // Passwords match, and date of birth is correct
+                res.redirect(`/dashboard`);
+            } else {
+                // Incorrect password or date of birth
+                res.send("Failed to sign in. Please check your email, password, and date of birth.");
+            }
+        } else {
+            // User with the provided email address not found
+            res.send("User not found. Please check your email address.");
+        }
+    } catch (error) {
+        console.error("Error querying Firestore:", error);
+        res.send("An error occurred. Please try again.");
+    }
+});
 
 
 
@@ -67,32 +145,38 @@ app.get("/login",function(req,res){
     res.sendFile(__dirname+"/views/"+"login.html");
 });
 
-
-
 app.get("/dashboard",function(req,res){
     res.sendFile(__dirname+"/views/"+"dashboard.html");
-
 });
- // Render the dashboard.ejs view});
-// Add a route to handle logout
+
 app.get("/logout",function(req,res){
     res.sendFile(__dirname+"/views/"+"logout.html");
-
 }); 
+
 app.get("/home",function(req,res){
     res.sendFile(__dirname+"/views/"+"home.html");
-
 }); 
+
 app.get("/about",function(req,res){
     res.sendFile(__dirname+"/views/"+"about.html");
-
 }); 
-// ... (your existing imports and code) ...
 
+app.get("/contact",function(req,res){
+    res.sendFile(__dirname+"/views/"+"contact.html");
+}); 
+
+app.get("/country",function(req,res){
+    res.sendFile(__dirname+"/views/"+"country.html");
+}); 
+
+app.get("/meal",function(req,res){
+    res.sendFile(__dirname+"/views/"+"meal.html");
+}); 
 
 app.get("/back", function(req, res) {
     res.redirect("/dashboard"); // Redirect to the dashboard route
 });
+
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}/login`);
+    console.log(`Server is running on http://localhost:${port}/home`);
 });
--- views/dashboard.js
+++ views/dashboard.js
@@ -1,44 +1,48 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const searchButton = document.getElementById('search-button');
-    const countryInput = document.getElementById('country-input');
-    const countryInfoElement = document.getElementById('country-info');
- 
-    searchButton.addEventListener('click', () => {
-        const countryName = countryInput.value;
-        console.log(countryName);
-      
-        // Fetch country data
-        fetch(`https://restcountries.com/v3.1/name/${countryName}`)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data); // Log the data received from the API
+const countrySearchButton = document.getElementById('country-search-button');
+const countryInput = document.getElementById('country-input');
+const countryInfoElement = document.getElementById('country-info');
 
-                if (data.length === 0) {
-                    countryInfoElement.innerHTML = `<p>No information found for "${countryName}".</p>`;
-                    return;
-                }
+function displayCountryData(data) {
+    countryInfoElement.innerHTML = ''; 
 
-                const countryData = data[0];
-                const capital = countryData.capital[0]; // Get the first capital if there are multiple
-                const borders = countryData.borders.join(', '); // Convert the array of borders to a string
+    if (data.length > 0) {
+        const countryData = data[0];
+        const capital = countryData.capital[0]; 
+        const borders = countryData.borders.join(', '); 
 
-                countryInfoElement.innerHTML = `
-                    <h2 style="color: red; ">${countryData.name.common}</h2>
-                    <p >Population: ${countryData.population}</p>
-                    <p>Languages: ${Object.values(countryData.languages).join(', ')}</p>
-                    <p>Currency: ${Object.keys(countryData.currencies).join(', ')}</p>
-                    <p>Region: ${countryData.region}</p> <!-- Display the region -->
-                    <p>Capital: ${capital}</p> <!-- Display capital city -->
-                    <p>Borders: ${borders}</p> <!-- Display bordering countries -->
+        countryInfoElement.innerHTML = `
+            <h2 style="color: red;">${countryData.name.common}</h2>
+            <p style="color:orangered;">Population: ${countryData.population}</p>
+            <p style="color:orangered;">Languages: ${Object.values(countryData.languages).join(', ')}</p>
+            <p style="color:orangered;">Currency: ${Object.keys(countryData.currencies).join(', ')}</p>
+            <p style="color:orangered;">Region: ${countryData.region}</p>
+            <p style="color:orangered;">Capital: ${capital}</p>
+            <p style="color:orangered;">Borders: ${borders}</p>
+        `;
+    } else {
+        countryInfoElement.innerHTML = `<p>No information found for "${countryInput.value}".</p>`;
+    }
+}
 
+function fetchAndDisplayCountryData(countryName) {
+    const apiUrl = `https://restcountries.com/v3.1/name/${countryName}`;
 
-                    <!-- Add more details as needed -->
-                `;
-            })
-            .catch(error => {
-                
-                console.error('Error fetching country data:', error);
-                countryInfoElement.innerHTML = `<p>An error occurred while fetching data.</p>`;
-            });
-    });
+    fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => {
+            displayCountryData(data);
+        })
+        .catch((error) => {
+            console.error('Error fetching country data:', error);
+            countryInfoElement.innerHTML = `<p>An error occurred while fetching data.</p>`;
+        });
+}
+
+countrySearchButton.addEventListener('click', () => {
+    const countryName = countryInput.value;
+    if (countryName) {
+        fetchAndDisplayCountryData(countryName);
+    }
 });
+
+
